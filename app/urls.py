a1849a897a5cd6a77c29a4223970c9ae568866f6
Enock24 <enookimuu@gmail.com>
2021-02-11 22:56:41 +0300

    add env

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,15 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.conf.urls import url,include
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.conf.urls import url,include
 
-from . import views
+# from . import views
 
-urlpatterns=[
-]
+# urlpatterns=[
+# ]
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+from .views import PostViewSet
+
+router = SimpleRouter()
+router.register('', PostViewSet, basename='posts')
+urlpatterns = router.urls
