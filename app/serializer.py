903f49af47e6873c77deaeb54682fe7c0e58ad38
abdisamad100 <abdisamadcade004@gmail.com>
2021-02-11 22:56:37 +0300

    created estate serializer

--- app/serializer.py
+++ app/serializer.py
@@ -1,1 +1,10 @@
 from rest_framework import serializers
+from .models import Estate
+
+
+class EstateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+        
