68243b959f0c4ff97f6ff88c1b4a1ab871f62dd5
Ian K <iankoech9@gmail.com>
2021-02-11 15:31:16 +0300

    Applied initial db migrations

--- app/models.py
+++ app/models.py
@@ -6,6 +6,18 @@ class Vendor(models.Model):
     Model creates user instances of vendors
     '''
     name = models.CharField(max_length=40)
+    email = models.EmailField()
+    contact = models.IntegerField()
     location = models.CharField(max_length=40)
     product_type = models.CharField(max_length=20)
-    
+    product_image = models.ImageField(upload_to = 'products/')
+    profile_pic = models.ImageField(upload_to = 'profile/')
+
+    def __str__(self):
+        return self.name
+
+    def saveVendor(self):
+        self.save()
+
+    def deleteVendor(self):
+        self.delete()  
