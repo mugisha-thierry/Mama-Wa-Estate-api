9f62802fb461ba30a3e56af0c237b3eb566a6dfd
Mugisha Thierry <thierrymugis@gmail.com>
2021-02-11 23:45:48 +0200

    Resolving conflicts

--- app/models.py
+++ app/models.py
@@ -2,6 +2,27 @@ from django.db import models
 import datetime as dt
 from django.contrib.auth.models import User
 # Create your models here.
+class Vendor(models.Model):
+    '''
+    Model creates user instances of vendors
+    '''
+    name = models.CharField(max_length=40)
+    email = models.EmailField()
+    contact = models.IntegerField()
+    location = models.CharField(max_length=40)
+    product_type = models.CharField(max_length=20)
+    product_image = models.ImageField(upload_to = 'products/')
+    profile_pic = models.ImageField(upload_to = 'profile/')
+
+    def __str__(self):
+        return self.name
+
+    def saveVendor(self):
+        self.save()
+
+    def deleteVendor(self):
+        self.delete()   
+
 class ProductMerch(models.Model):
     name = models.CharField(max_length=40)
     title = models.CharField(max_length=200,null=True,blank=True)
--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,14 @@
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from django.conf.urls import url,include
-
-# from . import views
-
-# urlpatterns=[
-# ]
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url,include
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 from .views import PostViewSet
+
+from . import views
+
+urlpatterns=[
+]
 
 router = SimpleRouter()
 router.register('', PostViewSet, basename='posts')
