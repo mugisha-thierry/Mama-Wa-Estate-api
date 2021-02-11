6720a0ae35f5a2c34c36414b4aa9129210505bbb
abdisamad100 <abdisamadcade004@gmail.com>
2021-02-11 22:04:35 +0300

    created model for the estates

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class Estate(models.Model):
+    name  = models.CharField(max_length=90)
+    location =models.CharField(max_length=800)
+    estate_logo = models.ImageField(upload_to="images",default="test.png")
+    description = models.TextField()
+   
+
+    def create_neighbourhood(self):
+        self.save()
+
+    def delete_neighbourhood(self):
+        self.delete()
+
+    def __str__(self):
+        return f"{self.name}"
