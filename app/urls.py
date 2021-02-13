21c04a88e62e80a30aec714205332c52a62ff760
Mugisha Thierry <thierrymugis@gmail.com>
2021-02-13 21:21:17 +0200

    removed un used codes

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url,include
 from django.urls import path
+from .views import VendorsList
 from rest_framework.routers import SimpleRouter
 from .views import PostViewSet
 
