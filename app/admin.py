c61f381521818d22b942ea8fb91ecaa37d71006c
Mugisha Thierry <thierrymugis@gmail.com>
2021-02-13 20:29:53 +0200

    Migrations

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Vendor 
+from .models import Vendor,Estate
 from .models import ProductMerch
 
 # Register your models here.
 admin.site.register(ProductMerch)
 admin.site.register(Vendor) 
+admin.site.register(Estate) 
