d8ec90ccd6a44056a227bc7cbfbfbf387c5aa19d
Ian K <iankoech9@gmail.com>
2021-02-18 11:49:22 +0300

    Added store

--- mama_wa_estate/settings.py
+++ mama_wa_estate/settings.py
@@ -68,7 +68,6 @@ MIDDLEWARE = [
 ]
 
 
-
 ROOT_URLCONF = 'mama_wa_estate.urls'
 
 TEMPLATES = [
@@ -184,7 +183,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
         
-    ]
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
         
