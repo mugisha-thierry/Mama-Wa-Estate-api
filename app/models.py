f4acde4a123d9831398b32d01e33f4c833f10cc1
Ian K <iankoech9@gmail.com>
2021-02-19 09:08:53 +0300

    Added post methods to api

--- app/models.py
+++ app/models.py
@@ -43,8 +43,8 @@ class Vendor(models.Model):
     username = models.CharField(max_length=30)
     email = models.EmailField()
     password = models.CharField(max_length=200)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
     
   
 
